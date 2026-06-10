8b7ccf14de5abc9fcd52b1031daa87d87602ff2d
Subzteveø <145073848+9TEVE-O@users.noreply.github.com>
2026-06-10 10:25:45 +0930

    [lab] add: LAB Safe Intake package init

---
+++ src/webwright/lab/__init__.py
@@ -0,0 +1,15 @@
+from webwright.lab.intake import (
+    TaskTier,
+    append_audit_log,
+    build_audit_record,
+    check_domain_allowlist,
+    classify_task,
+)
+
+__all__ = [
+    "TaskTier",
+    "append_audit_log",
+    "build_audit_record",
+    "check_domain_allowlist",
+    "classify_task",
+]
